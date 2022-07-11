df4302bd210ddaf982e7bede3fee7889b3144e64
bangroy <bangroy8420@gmail.com>
2022-07-11 23:17:08 +0700

    nav

--- index.php
+++ index.php
@@ -1,26 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <title>Toko Kita</title>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="css/style.css" rel="stylesheet">
-  <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-  <p>Menyediakan produk pakaian dan sarung </p>
-
-  <h1>isinya isinya</h1>
+<?php include 'header.php';  ?>
+<h1>selamat datang user</h1>
 
 
-  <h2>lana lana oooooo</h2>
-  <p>yyyy</p>
-  <p>okokokkokoko</p>
+<br>
 
-</body>
-
-</html>
-<?PHP
-?>
+<?php include 'footer.php';  ?>
